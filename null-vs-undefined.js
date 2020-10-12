8bc89a8046de4b13a82e7e07b6ffffb59f595686
israt faisal <israt.nsu141@gmail.com>
2020-10-12 19:23:49 -0400

    null v/s undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+return
+
+let result = add(13, 71);
+console.log(result);
+
+const boyfriend = {name:"smart dude", phone:"76839384"};
+console.log(boyfriend.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 4, 6]
+console.log(ages[7]);
