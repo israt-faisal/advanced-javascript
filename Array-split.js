1dbf744afc69f29beec783e136417ff612f79b48
israt faisal <israt.nsu141@gmail.com>
2020-10-19 22:22:34 -0400

    slice-splice

---
+++ Array-split.js
@@ -0,0 +1,13 @@
+const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(2, 7);
+
+const removed = nums.splice(2, 5);
+console.log(removed);
+console.log(nums);
+
+
+
+// if('false')
+// console.log("i am not false")
+// else
+// console.log("i am false")
